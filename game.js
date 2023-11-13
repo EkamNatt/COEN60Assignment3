fde1818d309807e8292d79dac01dadc4020e9539
EkamNatt <73764715+EkamNatt@users.noreply.github.com>
2023-11-13 13:16:34 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,27 @@
+// Prompt for the player's name
+const playerName = window.prompt("Please enter your name:");
+
+// Check if the player entered a name and it's not empty
+if (playerName) {
+    // Display the player's name and welcome message
+    const welcomeText = document.createElement("p");
+    welcomeText.textContent = `Welcome, ${playerName}!`;
+    
+    // Append the welcome message to the welcomeContainer div
+    const welcomeContainer = document.getElementById("welcomeContainer");
+    welcomeContainer.appendChild(welcomeText);
+} else {
+    // Handle the case where the player didn't enter a name
+    alert("Please enter your name to continue.");
+}
+
+// Game variables
 let totalPlays = 0;
 let paperCount = 0;
 let scissorsCount = 0;
 let rockCount = 0;
 
+// Play function
 function play() {
     let choice = document.querySelector('input[name="choice"]:checked');
     if (!choice) {
@@ -20,25 +39,29 @@ function play() {
     updateHistoryText();
 
     const result = compare(choice, computerChoice);
-    document.getElementById("computerResult").textContent = `Result: ${result}`;
+    document.getElementById("resultText").textContent = `Result: ${result}`;
 }
 
+// Function to get a random computer choice
 function getRandomChoice() {
     const choices = ["paper", "scissors", "rock"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+// Function to update play counts
 function updateCounts(choice) {
     if (choice === "paper") paperCount++;
     if (choice === "scissors") scissorsCount++;
     if (choice === "rock") rockCount++;
 }
 
+// Function to update history text
 function updateHistoryText() {
-    let historyText = `History: You have played total: ${totalPlays} time(s)! Paper: ${paperCount} time(s) & Scissors: ${scissorsCount} time(s) & Rock: ${rockCount} time(s).`;
-    document.getElementById("playHistory").textContent = historyText;
+    let historyText = `History: You have played a total of ${totalPlays} time(s)! Paper: ${paperCount} time(s) & Scissors: ${scissorsCount} time(s) & Rock: ${rockCount} time(s).`;
+    document.getElementById("historyText").textContent = historyText;
 }
 
+// Function to compare player and computer choices
 function compare(yourChoice, computerChoice) {
     if (yourChoice === computerChoice) return "It's a tie!";
     if ((yourChoice === "rock" && computerChoice === "scissors") || (yourChoice === "paper" && computerChoice === "rock") || (yourChoice === "scissors" && computerChoice === "paper")) return "You win!";
