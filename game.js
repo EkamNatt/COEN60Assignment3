9f72a87fcdb7faa162375754abc870067e455a00
EkamNatt <73764715+EkamNatt@users.noreply.github.com>
2023-11-13 13:06:10 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,39 +1,9 @@
-let playerName = "";
-let gameStarted = false;
 let totalPlays = 0;
 let paperCount = 0;
 let scissorsCount = 0;
 let rockCount = 0;
 
-function startGame() {
-    playerName = document.getElementById("playerName").value;
-    if (playerName === "") {
-        alert("Please enter your name.");
-    } else {
-        document.getElementById("welcomeText").textContent = `Welcome, ${playerName}! Let's play Rock Paper Scissors.`;
-        gameStarted = true;
-        document.getElementById("gameContainer").style.display = "block";
-    }
-}
-
-function getRandomChoice() {
-    const choices = ["paper", "scissors", "rock"];
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function updateHistoryText() {
-    document.getElementById("totalPlays").textContent = totalPlays;
-    document.getElementById("paperCount").textContent = paperCount;
-    document.getElementById("scissorsCount").textContent = scissorsCount;
-    document.getElementById("rockCount").textContent = rockCount;
-}
-
 function play() {
-    if (!gameStarted) {
-        alert("Please start the game first.");
-        return;
-    }
-
     let choice = document.querySelector('input[name="choice"]:checked');
     if (!choice) {
         alert("Please select Rock, Paper, or Scissors.");
@@ -42,36 +12,35 @@ function play() {
     choice = choice.value;
 
     const computerChoice = getRandomChoice();
-    document.getElementById("yourChoice").textContent = `You selected: ${choice}`;
-    document.getElementById("computerChoice").textContent = `Computer selected: ${computerChoice}`;
+    document.getElementById("yourChoice").textContent = `Your choice: ${choice}`;
+    document.getElementById("computerChoice").textContent = `Computer choice: ${computerChoice}`;
 
     totalPlays++;
-
-    if (choice === "paper") {
-        paperCount++;
-    } else if (choice === "scissors") {
-        scissorsCount++;
-    } else if (choice === "rock") {
-        rockCount++;
-    }
-
+    updateCounts(choice);
     updateHistoryText();
 
     const result = compare(choice, computerChoice);
-    document.getElementById("computerResult").textContent = result;
+    document.getElementById("computerResult").textContent = `Result: ${result}`;
+}
+
+function getRandomChoice() {
+    const choices = ["paper", "scissors", "rock"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function updateCounts(choice) {
+    if (choice === "paper") paperCount++;
+    if (choice === "scissors") scissorsCount++;
+    if (choice === "rock") rockCount++;
+}
+
+function updateHistoryText() {
+    let historyText = `History: You have played total: ${totalPlays} time(s)! Paper: ${paperCount} time(s) & Scissors: ${scissorsCount} time(s) & Rock: ${rockCount} time(s).`;
+    document.getElementById("playHistory").textContent = historyText;
 }
 
 function compare(yourChoice, computerChoice) {
-    if (yourChoice === computerChoice) {
-        return "It's a tie!";
-    } else if (
-        (yourChoice === "rock" && computerChoice === "scissors") ||
-        (yourChoice === "paper" && computerChoice === "rock") ||
-        (yourChoice === "scissors" && computerChoice === "paper")
-    ) {
-        return "You win!";
-    } else {
-        return "Computer wins!";
-    }
+    if (yourChoice === computerChoice) return "It's a tie!";
+    if ((yourChoice === "rock" && computerChoice === "scissors") || (yourChoice === "paper" && computerChoice === "rock") || (yourChoice === "scissors" && computerChoice === "paper")) return "You win!";
+    return "Computer wins!";
 }
-
